9603b8db3f3e01117b3b939e63909666fd3ad18a
malcano <steveleemh@naver.com>
2023-03-05 17:00:18 +0900

    button test

--- Flask/app.py
+++ Flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import config
 
 app = Flask(__name__)
@@ -10,7 +10,7 @@ cur = config.conn.cursor()
 # conn.commit()
 
 @app.route("/")
-def hello():
+def kkot():
     return render_template("index.html")
 @app.route('/result')
 def result():
--- Flask/config.py
+++ Flask/config.py
@@ -2,9 +2,9 @@ import pymysql
 
 conn = pymysql.connect(
     host = "deepl-db.cozq2mkojhyv.ap-northeast-2.rds.amazonaws.com",
-    port = "3306",
+    port = 3306,
     user = 'admin',
-    password  = 'alsh5600',
+    password  = 'alsgh5600',
     db = 'deepldb'
 )
 
