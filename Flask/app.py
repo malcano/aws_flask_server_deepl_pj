9acedc01ababcc0f05963eb6cad5bebdc6a06d0f
malcano <steveleemh@naver.com>
2023-03-06 16:16:44 +0900

    edit app.py redirect code and add final page html

--- Flask/app.py
+++ Flask/app.py
@@ -24,23 +24,31 @@ def kkot():
     return render_template("index.html")
 @app.route('/result/<input>')
 def result(input):#input: 사용자로부터 받는 메세지
+
+    #need to insert to Database
     sentence = input
     sentiment = int(md.sentiment_predict(input))
     circumstance = int(md.circumstance_predict(input))
+    rec_flower = getflower.fromFlowerList(sentiment, circumstance) # return value would be like [{"flower1":"sentence"},{"flower2":"sentence}]
     user_id = session.get("user_id")
 
-    rec_flower = getflower.fromFlowerList(sentiment, circumstance) # return value would be like [{"flower1":"sentence"},{"flower2":"sentence}]
-    # 둘 중 마음에 드는 꽃을 선택해주세요
+    #need to parse to select_page.html
+    first_flower = list(rec_flower[0].keys())[0]
+    second_flower = list(rec_flower[0].values())[0]
+    first_flower_explain = list(rec_flower[1].keys())[0]
+    second_flower_explain = list(rec_flower[1].values())[0]
 
-    sentence = "TEST MESSAGE: input message: " + str(input) +"\nsentiment predict: "+ getflower.getSentiment(sentiment) + \
-               "circumstance predict: "+getflower.getCircumstance(circumstance) + '\n\n' + "둘 중 더 마음에 드는 꽃을 선택해주세요.\n\n"+\
-                list(rec_flower[0].keys())[0]+": " + list(rec_flower[0].values())[0] + "\n"+ \
-                list(rec_flower[1].keys())[0]+": " + list(rec_flower[1].values())[0] + " unique_id : " + str(user_id)
+    # 둘 중 마음에 드는 꽃을 선택해주세요
+    #
+    # sentence = "TEST MESSAGE: input message: " + str(input) +"\nsentiment predict: "+ getflower.getSentiment(sentiment) + \
+    #            "circumstance predict: "+getflower.getCircumstance(circumstance) + '\n\n' + "둘 중 더 마음에 드는 꽃을 선택해주세요.\n\n"+\
+    #             list(rec_flower[0].keys())[0]+": " + list(rec_flower[0].values())[0] + "\n"+ \
+    #             list(rec_flower[1].keys())[0]+": " + list(rec_flower[1].values())[0] + " unique_id : " + str(user_id)
 
     # DB INSERT ID, SENTENCE, EMOTION, SITUATION
 
 
-    return sentence
+    return render_template("select_page.html")
 @app.route('/val')
 def validation():
     user_id = session.get("user_id")
@@ -62,6 +70,17 @@ def predict():
     else:
         temp = None
     return redirect(url_for('predict',input=input))
+
+@app.route('/val', methods=['POST','GET'])
+def save():
+    if request.method == 'POST':
+        input = request.form['input']
+        n_unique = id.newID()
+        print(input)
+        return redirect(url_for('result', input=input))
+    else:
+        temp = None
+
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
